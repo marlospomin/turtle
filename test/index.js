96d2412168be1f1693ca6fd09e56de77c2d5da15
Marlos Pomin <marlospomin@gmail.com>
2017-10-08 22:09:45 -0300

    Add tests and jsdom

--- test/index.js
+++ test/index.js
@@ -1,3 +1,27 @@
-const assert = require('assert');
+require('jsdom-global')()
 
-// Code
+const assert = require('assert');
+const turtle = require('../src/turtle.js');
+
+describe('turtle', function () {
+  describe('intersection observer', function () {
+    it('should be loaded', function () {
+      // assert('IntersectionObserver' in window);
+      assert.equal('function', typeof IntersectionObserver);
+    });
+  });
+
+  describe('images containing appropriated class', function () {
+    beforeEach(function () {
+      const image = document.createElement('img');
+      image.dataset.src = Math.random().toString(36).substring(7);
+      image.classList.add('lazyload');
+      document.body.appendChild(image);
+    })
+
+    it('should be loaded', function () {
+      const image = document.querySelector('.lazyload');
+      assert(image.classList.contains('.lazyload--handled'));
+    });
+  });
+});

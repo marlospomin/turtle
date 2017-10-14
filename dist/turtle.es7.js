f59c1a28d4edbc5282c1b21a4a0cf2bc55737190
Marlos Pomin <marlospomin@gmail.com>
2017-10-13 21:43:05 -0300

    Update turtle

--- dist/turtle.es7.js
+++ dist/turtle.es7.js
@@ -1,97 +1,97 @@
-// Actual plugin below
 // Get all of the images that are marked up to lazy load
 const images = document.querySelectorAll('.turtle');
 const config = {
-  // If the image gets within 50px in the Y axis, start the download.
+  // If the image gets within 50px of the Y axis, start the download
   rootMargin: '50px 0px',
   threshold: 0
 };
 
-// Counts all the images found that were marked
-let imageCount = images.length;
-let observer;
+export default function turtle() {
+  // Counts all the images found that were marked
+  let imageCount = images.length;
+  let observer;
 
-// If we don't have support for intersection observer, loads the images immediately
-if (!('IntersectionObserver' in window)) {
-  loadImagesImmediately(images);
-} else {
-  // If supported, load the images
-  observer = new IntersectionObserver(onIntersection, config);
+  // If we don't have support for intersection observer, loads the images immediately
+  if (!('IntersectionObserver' in window)) {
+    loadImagesImmediately(images);
+  } else {
+    // If it is supported, load the images
+    observer = new IntersectionObserver(onIntersection, config);
 
-  // foreach() is not supported in IE < 11
-  for (let i = 0; i < images.length; i++) {
-    let image = images[i];
-    if (image.classList.contains('turtle--handled')) {
-      continue;
+    // foreach() is not supported in IE < 11
+    images.forEach((image) => {
+      // If the image has been handled, skip to the next image
+      if (image.classList.contains('turtle--handled')) {
+        return;
+      }
+
+      // Observes the image
+      observer.observe(image);
+    });
+  }
+
+  function fetchImage(url) {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+      image.src = url;
+      image.onload = resolve;
+      image.onerror = reject;
+    });
+  }
+
+  function preloadImage(image) {
+    const src = image.dataset.src;
+
+    // If src is not found break
+    if (!src) {
+      return;
     }
 
-    // Observes the image
-    observer.observe(image);
-  }
-}
-
-function fetchImage(url) {
-  return new Promise((resolve, reject) => {
-    const image = new Image();
-    image.src = url;
-    image.onload = resolve;
-    image.onerror = reject;
-  });
-}
-
-function preloadImage(image) {
-  const src = image.dataset.src;
-
-  // If src is not found break
-  if (!src) {
-    return;
+    // Fetches an image and applies it to the viewport
+    return fetchImage(src).then(() => { applyImage(image, src); });
   }
 
-  return fetchImage(src).then(() => { applyImage(image, src); });
-}
-
-function loadImagesImmediately(images) {
-  // foreach() is not supported in IE < 11
-  for (let i = 0; i < images.length; i++) {
-    let image = images[i];
-    preloadImage(image);
-  }
-}
-
-function disconnect() {
-  // If not observing break
-  if (!observer) {
-    return;
+  function loadImagesImmediately(images) {
+    // foreach() is not supported in IE < 11
+    images.forEach((image) => {
+      preloadImage(image);
+    });
   }
 
-  observer.disconnect();
-}
-
-function onIntersection(entries) {
-  // Disconnect if we've already loaded all of the images
-  if (imageCount === 0) {
-    disconnect();
-  }
-
-  // Loop through the entries
-  for (let i = 0; i < entries.length; i++) {
-    let entry = entries[i];
-    // Are we in viewport?
-    if (entry.intersectionRatio > 0) {
-      imageCount--;
-
-      // Stop watching and load the image
-      observer.unobserve(entry.target);
-      preloadImage(entry.target);
+  function disconnect() {
+    // If not observing break
+    if (!observer) {
+      return;
     }
+
+    observer.disconnect();
+  }
+
+  function onIntersection(entries) {
+    // Disconnect if we've already loaded all of the images
+    if (imageCount === 0) {
+      disconnect();
+    }
+
+    // Loop through image entries
+    entries.forEach((entry) => {
+      // If the image is in the viewport unobserve it
+      if (entry.intersectionRatio > 0) {
+        imageCount--;
+
+        // Stop watching and load the image
+        observer.unobserve(entry.target);
+        preloadImage(entry.target);
+      }
+    });
+  }
+
+  function applyImage(img, src) {
+    // Prevent this from being lazy loaded a second time
+    img.classList.add('turle--handled');
+    // Update image src value
+    img.src = src;
   }
 }
 
-function applyImage(img, src) {
-  // Prevent this from being lazy loaded a second time.
-  img.classList.add('turle--handled');
-  // Update image src value
-  img.src = src;
-  // Add fade-in effect handled by the user
-  img.classList.add('fade-in');
-}
+turtle();
--- dist/turtle.js
+++ dist/turtle.js
@@ -1,101 +1,108 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-// Actual plugin below
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = turtle;
 // Get all of the images that are marked up to lazy load
 var images = document.querySelectorAll('.turtle');
 var config = {
-  // If the image gets within 50px in the Y axis, start the download.
+  // If the image gets within 50px of the Y axis, start the download
   rootMargin: '50px 0px',
   threshold: 0
 };
 
-// Counts all the images found that were marked
-var imageCount = images.length;
-var observer = void 0;
+function turtle() {
+  // Counts all the images found that were marked
+  var imageCount = images.length;
+  var observer = void 0;
 
-// If we don't have support for intersection observer, loads the images immediately
-if (!('IntersectionObserver' in window)) {
-  loadImagesImmediately(images);
-} else {
-  // If supported, load the images
-  observer = new IntersectionObserver(onIntersection, config);
+  // If we don't have support for intersection observer, loads the images immediately
+  if (!('IntersectionObserver' in window)) {
+    loadImagesImmediately(images);
+  } else {
+    // If it is supported, load the images
+    observer = new IntersectionObserver(onIntersection, config);
 
-  // foreach() is not supported in IE < 11
-  for (var i = 0; i < images.length; i++) {
-    var image = images[i];
-    if (image.classList.contains('turtle--handled')) {
-      continue;
+    // foreach() is not supported in IE < 11
+    images.forEach(function (image) {
+      // If the image has been handled, skip to the next image
+      if (image.classList.contains('turtle--handled')) {
+        return;
+      }
+
+      // Observes the image
+      observer.observe(image);
+    });
+  }
+
+  function fetchImage(url) {
+    return new Promise(function (resolve, reject) {
+      var image = new Image();
+      image.src = url;
+      image.onload = resolve;
+      image.onerror = reject;
+    });
+  }
+
+  function preloadImage(image) {
+    var src = image.dataset.src;
+
+    // If src is not found break
+    if (!src) {
+      return;
     }
 
-    // Observes the image
-    observer.observe(image);
-  }
-}
-
-function fetchImage(url) {
-  return new Promise(function (resolve, reject) {
-    var image = new Image();
-    image.src = url;
-    image.onload = resolve;
-    image.onerror = reject;
-  });
-}
-
-function preloadImage(image) {
-  var src = image.dataset.src;
-
-  // If src is not found break
-  if (!src) {
-    return;
+    // Fetches an image and applies it to the viewport
+    return fetchImage(src).then(function () {
+      applyImage(image, src);
+    });
   }
 
-  return fetchImage(src).then(function () {
-    applyImage(image, src);
-  });
-}
-
-function loadImagesImmediately(images) {
-  // foreach() is not supported in IE < 11
-  for (var _i = 0; _i < images.length; _i++) {
-    var _image = images[_i];
-    preloadImage(_image);
-  }
-}
-
-function disconnect() {
-  // If not observing break
-  if (!observer) {
-    return;
+  function loadImagesImmediately(images) {
+    // foreach() is not supported in IE < 11
+    images.forEach(function (image) {
+      preloadImage(image);
+    });
   }
 
-  observer.disconnect();
-}
-
-function onIntersection(entries) {
-  // Disconnect if we've already loaded all of the images
-  if (imageCount === 0) {
-    disconnect();
-  }
-
-  // Loop through the entries
-  for (var _i2 = 0; _i2 < entries.length; _i2++) {
-    var entry = entries[_i2];
-    // Are we in viewport?
-    if (entry.intersectionRatio > 0) {
-      imageCount--;
-
-      // Stop watching and load the image
-      observer.unobserve(entry.target);
-      preloadImage(entry.target);
+  function disconnect() {
+    // If not observing break
+    if (!observer) {
+      return;
     }
+
+    observer.disconnect();
+  }
+
+  function onIntersection(entries) {
+    // Disconnect if we've already loaded all of the images
+    if (imageCount === 0) {
+      disconnect();
+    }
+
+    // Loop through image entries
+    entries.forEach(function (entry) {
+      // If the image is in the viewport unobserve it
+      if (entry.intersectionRatio > 0) {
+        imageCount--;
+
+        // Stop watching and load the image
+        observer.unobserve(entry.target);
+        preloadImage(entry.target);
+      }
+    });
+  }
+
+  function applyImage(img, src) {
+    // Prevent this from being lazy loaded a second time
+    img.classList.add('turle--handled');
+    // Update image src value
+    img.src = src;
   }
 }
 
-function applyImage(img, src) {
-  // Prevent this from being lazy loaded a second time.
-  img.classList.add('turle--handled');
-  // Update image src value
-  img.src = src;
-  // Add fade-in effect handled by the user
-  img.classList.add('fade-in');
-}
+turtle();
+module.exports = exports['default'];
+},{}]},{},[1])

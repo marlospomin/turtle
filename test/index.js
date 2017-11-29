6bc0a7427f4411e0910a2f6b044e7167908e57a8
Marlos Pomin <marlospomin@gmail.com>
2017-11-29 19:22:55 -0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -7,32 +7,11 @@ describe('turtle', () => {
     });
   });
 
-  // Actual tests
   describe('source code', () => {
     it('should be a function', () => {
       assert(window.turtle);
     });
   });
 
-  describe('images containing the appropriated class', () => {
-    before(() => {})
-
-    it('should be loaded', () => {});
-  });
-
-  describe('images not containing the appropriated class', () => {
-    before(() => {})
-
-    it('should not be loaded', () => {
-
-    });
-  });
-
-  describe('images containing the appropriated data attribute', () => {
-    before(() => {})
-
-    it('should be loaded', () => {
-
-    });
-  });
+  // Actual tests below
 });

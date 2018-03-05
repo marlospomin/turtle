eb2ddd6b9fc0ab371cf5afdcdcc711d9cfab81ca
Marlos Pomin <marlospomin@gmail.com>
2018-03-05 15:03:40 -0300

    Drop commas

--- test/index.js
+++ test/index.js
@@ -1,17 +1,14 @@
-// Turtle tests
 describe('turtle', () => {
   // "Just to make sure" tests
   describe('intersection observer', () => {
     it('should be supported', () => {
       assert(window.IntersectionObserver);
-    });
-  });
+    })
+  })
 
   describe('source code', () => {
     it('should be a function', () => {
       assert(window.turtle);
-    });
-  });
-
-  // Actual tests below
-});
+    })
+  })
+})
